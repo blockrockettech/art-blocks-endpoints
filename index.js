50df6494c53da07fb066449d5d254e0e8ee80be7
Andy Gray <andy@whatsthescore.co.uk>
2018-07-29 10:43:22 +0100

    token endpoints (more)

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const app = express();
 
 app.use(cors());
 
-app.get('/tokn/:network/:contractAddress/:tokenId', function (req, res) {
+app.get('/tokn/:network/:contractAddress/tokenid/:tokenId', function (req, res) {
 
   const address = req.params.contractAddress;
   const network = req.params.network;
@@ -46,6 +46,7 @@ app.get('/tokn/:network/:contractAddress/:tokenId', function (req, res) {
     contract.nicknameOf(tokenId),
     contract.ownerOf(tokenId),
     contract.tokenURI(tokenId),
+    contract.getApproved(tokenId),
   ])
     .then((result) => {
       res.json({
@@ -53,6 +54,31 @@ app.get('/tokn/:network/:contractAddress/:tokenId', function (req, res) {
         nickname: result[1].toString(10),
         owner: result[2].toString(10),
         tokenURI: result[3].toString(10),
+        approved: result[4].toString(10),
+      });
+    })
+    .catch(err => console.log(err));
+});
+
+app.get('/tokn/:network/:contractAddress/owner/:owner', function (req, res) {
+
+  const address = req.params.contractAddress;
+  const network = req.params.network;
+  const owner = req.params.owner;
+  const contract = new ethers.Contract(address, toknAbi, getProvider(network));
+
+  return Promise.all([
+    contract.hasTokens(owner),
+    contract.tokensOf(owner),
+    contract.firstToken(owner),
+    contract.balanceOf(owner),
+  ])
+    .then((result) => {
+      res.json({
+        hasTokens: result[0].toString(10),
+        tokens: result[1].toString(10),
+        firstToken: result[2].toString(10),
+        balance: result[3].toString(10),
       });
     })
     .catch(err => console.log(err));
